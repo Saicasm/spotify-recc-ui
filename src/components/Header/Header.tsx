65db2fc85efef55c84b931ef10c207523afd3fc4
Sai Kumar N <sai98nsk@gmail.com>
2024-04-19 14:01:14 -0400

    feat: added header for the project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "../Button/Button";
+
+interface HeaderProps {
+
+  /**
+   * The Title of the APP
+   */
+  title: string;
+  /**
+   * Additional classes to be added to the Header
+   */
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({
+  title = "Musicfic",
+  className = "",
+}) => {
+//TODO: Added General Layout of the header, need to finish it
+  return (
+    <div className="bg-light-bg-primary h-16 border-b-2 border-black flex justify-center items-center">
+    <div className="">{title}</div>
+    <div>Search Songs</div>
+    <div><Button variant="primary" >Search</Button></div>
+    <div>User Profile</div>
+  </div>
+  );
+};
+
+export default Header;
