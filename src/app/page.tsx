3050f493b371c5565ff321f41b221ce5210951e2
Sai Kumar N <sai98nsk@gmail.com>
2024-04-19 14:00:39 -0400

    fix: cleaned up landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import Image from "next/image";
-import Button from "@/components/Button/Button";
-import CustomSelect from "@/components/Select/Select";
 import { useState } from "react";
-import Card from "@/components/Card/Card";
 
 export default function Home() {
   const options = [
@@ -21,31 +17,9 @@ export default function Home() {
   const handleClick = () => {
     console.log("Button clicked!");
   };
+  //TODO: Finish landing page 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-light-bg-primary bg-opacity-90'>
-      <Card
-        title='Sample Card Title'
-        description='This is a sample card component.'
-        imageUrl='https://as2.ftcdn.net/v2/jpg/05/08/37/21/1000_F_508372195_H9K3KIdsqlVMDgPd0ENTa8TUXuq9L9jF.jpg'
-        className='bg-light-accent-primary '
-      />
-      <Button onClick={handleClick} variant='primary'>
-        Primary Button
-      </Button>
-      <Button onClick={handleClick} variant='secondary'>
-        Secondary Button
-      </Button>
-      {/* <Button onClick={handleClick} className='bg-blue-500'>
-        Custom variant Button
-      </Button> */}
-      {/* <div>
-        <CustomSelect
-          options={options}
-          onChange={handleChange}
-          placeholder='Select an option'
-          className='border-red-500 dark:accent-primary' // Example of additional Tailwind classes for customization
-        />
-      </div> */}
     </main>
   );
 }
