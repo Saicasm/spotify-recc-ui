fd3dbcf98a7867f59379b92f9e4f8f99f86ae803
Sai Kumar N <sai98nsk@gmail.com>
2024-04-20 00:18:45 -0400

    dcos: spelling fix

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -9,7 +9,7 @@ const meta = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  // This component will have an automatically generated AutoDoc entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   argTypes: {
     onClick: { action: "clicked" },
