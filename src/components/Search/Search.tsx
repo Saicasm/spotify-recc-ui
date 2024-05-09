8d4b044c66e8f90aa32fbf9f5d7cdd17ea468767
Sai Kumar N <sai98nsk@gmail.com>
2024-05-09 15:58:04 -0400

    refactor: added generic type for search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 
-interface SearchBarProps {
+interface SearchBarProps<T> {
   placeholder?: string;
   className?: string;
-  items?: string[];
+  items?: T[];
+  displayKey: keyof T;
   onSearchQueryChange?: (query: string) => void;
-  onSearchSelect?: (query: string) => void;
+  onSearchSelect?: (item: T) => void;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({
+const SearchBar= <T,>({
   placeholder = "",
   className = "",
-  items = [] as string[],
+  items = [],
+  displayKey,
   onSearchQueryChange,
   onSearchSelect
-}) => {
+}:SearchBarProps<T> ) => {
   const [query, setQuery] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
@@ -27,8 +29,9 @@ const SearchBar: React.FC<SearchBarProps> = ({
     }  
   };
 
-  const handleDropdownItemClick = (item: string) => {
+  const handleDropdownItemClick = (item: T) => {
     // Handle dropdown item click action
+    console.log(item);
     onSearchSelect?.(item);
     setIsOpen(false); // Close dropdown after item selection
   };
@@ -43,17 +46,17 @@ const SearchBar: React.FC<SearchBarProps> = ({
         className="py-2 px-4 rounded-sm  border outline-none focus:border-light-border-primary  w-60"
       />
       {isOpen && (
-        <div className="absolute top-full left-0 w-full bg-light-bg-secondary border border-gray-200 rounded-lg shadow-md mt-2">
-          {items?.map((item, index) => (
-            <div
-              key={index}
-              onClick={() => handleDropdownItemClick(item)}
-              className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
-            >
-              {item}
-            </div>
-          ))}
-        </div>
+       <div className="absolute top-full left-0 w-full bg-light-bg-secondary border border-gray-200 rounded-lg shadow-md mt-2 z-10">
+       {items.map((item, index) => (
+         <div
+           key={index}
+           onClick={() => handleDropdownItemClick(item)}
+           className="py-2 px-4 hover:bg-gray-100 cursor-pointer "
+         >
+           {item[displayKey]as React.ReactNode}
+         </div>
+       ))}
+     </div>
       )}
     </div>
   );
